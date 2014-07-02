81fed152c5b2de9d95862b5dc65e63c827c8b0d3
Jordi Llobet <jordi@kentlyons.com>
2014-07-02 12:12:19 +0100

    Adding cross origin headers

--- app.js
+++ app.js
@@ -35,14 +35,13 @@ app.use(flash());
 require('./app/routes.js')(router, passport);
 
 // ROUTES
-/*
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
-*/
+
 
 
 // STARTUP SERVER
