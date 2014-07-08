c80b6ff06bc83ee2ef02d6d91a6e92b1d02763da
Jordi Llobet <jordi@kentlyons.com>
2014-07-08 14:35:21 +0100

    Add new user with FB

--- app/routes.js
+++ app/routes.js
@@ -7,36 +7,60 @@ var tokenManager    = require('../config/token_manager');
 
 exports.register = function(req, res) {
     console.log("Register...");
-    //console.log(req.body);
+    console.log(req);
     console.log("------------------------------------------------");
     
+    // GET THE INFORMATION
+    var info    = req.body.profile;
+    var sn      = req.body.social;
 
-    switch(req.body.sn) {
+    switch(sn) {
 
-        case 'fb':
+        case 'facebook':
             console.log("FB Login");
             
-            
-            var id = req.body.facebook.id;
-            var user = new db.userModel();
-            /*
-            db.userModel.findOne({ facebook.id : id}, function(err, user) {
-                if (err) {
-                    console.log(err);
+            // 1. CHECK DE [EMAIL] AND [SOCIAL]
+                if (info) {
+                    db.userModel.findOne({ "facebook.email" : info.email}, function(err, user) {
+                        if (err)
+                            console.log(err);
+                            return res.send(401);
+                        if (!user) {
+                            // 2.1. IF !EXIST (IT'S NEW)
+                                // 2.1.1. REGISTER
+                                var user = new db.userModel();
+                                var seen = [];
+                                JSON.stringify(profile, function(key, val) {
+                                    if (val != null && typeof val == "object") {
+                                        if (seen.indexOf(val) >= 0)
+                                            return
+                                        seen.push(val)
+                                    }
+                                    return val;
+                                });
+                                user.local = seen;
+                                var token = jwt.sign({id: user._id}, secret.secretToken, { expiresInMinutes: tokenManager.TOKEN_EXPIRATION });
+                                user.access_token = token;
+                                user.save();
+                                return res.send(200, { token : token, message : "User created succesfully!" });
+
+                                // 2.1.2. CREATE TOKEN
+                                // 2.1.3. RETURN TOKEN
+                        }
+                        if (user) {
+                            console.log(".........This userr already exist");
+                        }
+                        
+                        // 2.2. IF EXIST
+                            // 2.2.1. RECEIVING [ACCESS_TOKEN]
+                                // 2.2.1.1 MERGE ACCOUNTS [EMAIL] [SOCIAL]
+                            // 2.2.2. !RECEIVING [ACCESS_TOKEN]
+                                // 2.2.2.1 SIMPLE LOG IN [EMAIL] [SOCIAL] 
+
+                    });
+                }else{
+                    return res.send(400, { message : "Error" });
                 }
-                return res.send(200);
-            });
-            var seen = [];
-            JSON.stringify(profile, function(key, val) {
-                if (val != null && typeof val == "object") {
-                    if (seen.indexOf(val) >= 0)
-                        return
-                    seen.push(val)
-                }
-                return val
-            });
-            user.local = seen;
-            */
             break;
 
         case 'tw':
