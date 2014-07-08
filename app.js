726b0b3bf0246ad902b6f19a4c79d95f75eed691
Jordi Llobet <jordi@kentlyons.com>
2014-07-08 15:03:06 +0100

    Adding CORS for everything

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ routes.users    = require('./app/routes.js');
 
 
 app.all('*', function(req, res, next) {
-  res.set('Access-Control-Allow-Origin', 'http://localhost');
+  res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Credentials', true);
   res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
