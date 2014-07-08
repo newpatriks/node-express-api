6694172a8138e0af226048cde1272b86a4544fdc
Jordi Llobet <jordi@kentlyons.com>
2014-07-08 17:43:40 +0100

    Putting some order in files/folders

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var port        = process.env.PORT || 5000;
 var jwt         = require('express-jwt');
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
+var tokenManager= require('./config/token_manager');
+var secret      = require('./config/secret');
+
 
 // STARTUP SERVER
 app.listen(port);
@@ -11,79 +14,16 @@ app.use(bodyParser());
 app.use(morgan());
 
 var routes      = {};
-routes.users    = require('./app/routes.js');
+routes.users    = require('./route/users.js');
 
 
 app.all('*', function(req, res, next) {
-  res.set('Access-Control-Allow-Origin', 'http://localhost');
-  res.set('Access-Control-Allow-Origin', 'http://jsfiddle.net/');
-  res.set('Access-Control-Allow-Credentials', true);
-  res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
-  if ('OPTIONS' == req.method) return res.send(200);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
   next();
 });
 
+
 app.post('/user/register', routes.users.register);
-
-/*
-var express     = require('express');
-var app         = express();
-var port        = process.env.PORT || 5000;
-var mongoose    = require('mongoose');
-var passport    = require('passport');
-var flash       = require('connect-flash');
-var jwt         = require('jwt-simple');
-var morgan      = require('morgan');
-var cookieParser= require('cookie-parser');
-var bodyParser  = require('body-parser');
-var session     = require('express-session');
-
-var uristring   = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/lockedin';
-mongoose.connect(uristring, function(err, res) {
-    if (err) {
-        console.log("ERROR MONGODB");
-    }else{
-        console.log("SUCCEEDED CONNECTING MONGODB");
-    }
-});
-//require('./config/passport')(passport, router, app, jwt);
-
-var routes = {};
-routes.users = requre('./route/users.')
-
-
-app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-app.set('jwtTokenSecret', 'k3ntly0n5rulz');
-app.set('view engine', 'ejs');
-
-app.use(session({ secret : 'ilovescotchscotchyscotchscotch' }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-
-// ROUTES
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-require('./app/routes.js')(router, passport);
-
-
-app.post('/user/register', routes.users.register); 
-
-
-// STARTUP SERVER
-app.use('/api', router);
-app.listen(port, function() {
-    console.log("Listening on port number: ", port);
-});
-*/
+app.get('/users/all', jwt({secret: secret.secretToken}), tokenManager.verifyToken, routes.users.listAll);
---
+++ route/users.js
@@ -0,0 +1,105 @@
+var db              = require('../config/mongo_db');
+var jwt             = require('jsonwebtoken');
+var secret          = require('../config/secret');
+var redisClient     = require('../config/redis_db').redisClient;
+var tokenManager    = require('../config/token_manager');
+
+
+exports.register = function(req, res) {
+    console.log("Register...");
+    console.log("------------------------------------------------");
+    
+    // GET THE INFORMATION
+    var info    = req.body.profile || '';
+    var sn      = req.body.social || '';
+
+    console.log("info ------------------");
+    console.log(info);
+
+    console.log("sn --------------------");
+    console.log(sn);
+
+
+    switch(sn) {
+
+        case 'facebook':
+            console.log("FB Login");
+            
+            // 1. CHECK DE [EMAIL] AND [SOCIAL]
+                if (info) {
+                    db.userModel.findOne({ 'facebook.email' : info.email}, function(err, user) {
+                        if (err)
+                            return res.send(401, {message : err});
+                        if (!user) {
+                        // 2.1. IF !EXIST (IT'S NEW)
+                            // 2.1.1. REGISTER
+                            var user = new db.userModel();
+                            var seen = [];
+                            JSON.stringify(profile, function(key, val) {
+                                if (val != null && typeof val == "object") {
+                                    if (seen.indexOf(val) >= 0)
+                                        return
+                                    seen.push(val)
+                                }
+                                return val;
+                            });
+                            user.local = seen;
+                            var token = jwt.sign({id: user._id}, secret.secretToken, { expiresInMinutes: tokenManager.TOKEN_EXPIRATION });
+                            user.access_token = token;
+                            user.save();
+                            return res.send(200, { token : token });
+
+                            // 2.1.2. CREATE TOKEN
+                            // 2.1.3. RETURN TOKEN
+                        }
+                        if (user) {
+                            console.log(".........This userr already exist");
+                        }
+                        
+                        // 2.2. IF EXIST
+                            // 2.2.1. RECEIVING [ACCESS_TOKEN]
+                                // 2.2.1.1 MERGE ACCOUNTS [EMAIL] [SOCIAL]
+                            // 2.2.2. !RECEIVING [ACCESS_TOKEN]
+                                // 2.2.2.1 SIMPLE LOG IN [EMAIL] [SOCIAL] 
+
+                    });
+                }else{
+                    return res.send(400, { message : "Error" });
+                }
+            break;
+
+        case 'tw':
+            console.log("TW Login");
+            break;
+
+        case 'g+':
+            console.log("G+ Login");
+            break;
+
+        case 'bbc':
+            console.log("BBC Login");
+            break;
+    }
+}
+
+
+
+exports.listAll = function(req, res) {
+    console.log("List All --------------");
+    if (!req.user) 
+        return res.send(401);
+
+    var query = db.userModel.find();
+    query.sort('-created');
+    query.exec(function(err, results) {
+        if (err) {
+            console.log(err);
+            return res.send(400);
+        }
+        for (var postKey in results) {
+            results[postKey].content = results[postKey].content.substr(0, 400);
+        }
+
+        return res.json(200, results);
+    });
+};
