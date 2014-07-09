00e56dfde19755ca88fb511b089808d6782a0b02
Jordi Llobet <jordi@kentlyons.com>
2014-07-09 14:17:54 +0100

    Some errors only on the server when is trying to list all users

--- route/users.js
+++ route/users.js
@@ -26,58 +26,46 @@ exports.register = function(req, res) {
             console.log("FB Login");
             var info    = req.body.facebook || '';
             // 1. CHECK DE [EMAIL] AND [SOCIAL]
-                if (info) {
-                    db.userModel.findOne({ 'facebook.email' : info.email}, function(err, user) {
-                        if (err)
-                            return res.send(401, {message : err});
-                        if (!user) {
-                        // 2.1. IF !EXIST (IT'S NEW)
-                            // 2.1.1. REGISTER
-                            var user = new db.userModel();
-                            var seen = [];
-                            JSON.stringify(info, function(key, val) {
-                                if (val != null && typeof val == "object") {
-                                    if (seen.indexOf(val) >= 0)
-                                        return
-                                    seen.push(val)
-                                }
-                                return val;
-                            });
-                            user.local = seen;
-                            var token = jwt.sign({id: user._id}, secret.secretToken, { expiresInMinutes: tokenManager.TOKEN_EXPIRATION });
-                            user.access_token = token;
-                            user.save();
-                            return res.send(200, { token : token });
+            if (info) {
+                db.userModel.findOne({ 'facebook.email' : info.email}, function(err, user) {
+                    if (err)
+                        return res.send(401, {message : err});
+                    if (!user) {
+                    // 2.1. IF !EXIST (IT'S NEW)
+                        // 2.1.1. REGISTER
+                        var user = new db.userModel();
+                        var seen = [];
+                        JSON.stringify(info, function(key, val) {
+                            if (val != null && typeof val == "object") {
+                                if (seen.indexOf(val) >= 0)
+                                    return
+                                seen.push(val)
+                            }
+                            return val;
+                        });
+                        user.local = seen;
+                        var token = jwt.sign({id: user._id}, secret.secretToken, { expiresInMinutes: tokenManager.TOKEN_EXPIRATION });
+                        user.access_token = token;
+                        user.save();
+                        return res.send(200, { token : token });
 
-                            // 2.1.2. CREATE TOKEN
-                            // 2.1.3. RETURN TOKEN
-                        }
-                        if (user) {
-                            console.log(".........This userr already exist");
-                        }
-                        
-                        // 2.2. IF EXIST
-                            // 2.2.1. RECEIVING [ACCESS_TOKEN]
-                                // 2.2.1.1 MERGE ACCOUNTS [EMAIL] [SOCIAL]
-                            // 2.2.2. !RECEIVING [ACCESS_TOKEN]
-                                // 2.2.2.1 SIMPLE LOG IN [EMAIL] [SOCIAL] 
+                        // 2.1.2. CREATE TOKEN
+                        // 2.1.3. RETURN TOKEN
+                    }
+                    if (user) {
+                        console.log(".........This userr already exist");
+                    }
+                    
+                    // 2.2. IF EXIST
+                        // 2.2.1. RECEIVING [ACCESS_TOKEN]
+                            // 2.2.1.1 MERGE ACCOUNTS [EMAIL] [SOCIAL]
+                        // 2.2.2. !RECEIVING [ACCESS_TOKEN]
+                            // 2.2.2.1 SIMPLE LOG IN [EMAIL] [SOCIAL] 
 
-                    });
-                }else{
-                    return res.send(400, { message : "Error" });
-                }
-            break;
-
-        case 'tw':
-            console.log("TW Login");
-            break;
-
-        case 'g+':
-            console.log("G+ Login");
-            break;
-
-        case 'bbc':
-            console.log("BBC Login");
+                });
+            }else{
+                return res.send(400, { message : "Error" });
+            }
             break;
     }
 }
@@ -90,7 +78,7 @@ exports.listAll = function(req, res) {
         return res.send(401);
 
     var query = db.userModel.find();
-    query.sort('-created');
+    //query.sort('-created');
     query.exec(function(err, results) {
         if (err) {
             console.log(err);
