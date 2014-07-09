ee7793311e17d6313230b0fd87af65a9554cec0b
Jordi Llobet <jordi@kentlyons.com>
2014-07-09 14:41:52 +0100

    Configuring Redis conection

--- config/redis_db.js
+++ config/redis_db.js
@@ -1,5 +1,19 @@
-var redis = require('redis');
-var redisClient = redis.createClient(6379);
+var redis       = require('redis');
+var redisPort   = 10118;
+var heroku = [];
+
+if (process.env.REDISTOGO_URL) {
+    // use production (Heroku) redis configuration
+    // overwrite config to keep it simple
+    var rtg             = require('url').parse(process.env.REDISTOGO_URL);
+    heroku.port         = rtg.port;
+    heroku.host         = rtg.hostname;
+    heroku.password     = rtg.auth.split(':')[1];
+}
+
+var port = heroku.port || ''; 
+var host = heroku.host || '';
+var redisClient = redis.createClient(port, host);
 
 redisClient.on('error', function (err) {
     console.log('Error ' + err);
@@ -9,5 +23,3 @@ redisClient.on('connect', function () {
     console.log('Redis is ready');
 });
 
-exports.redis = redis;
-exports.redisClient = redisClient;
