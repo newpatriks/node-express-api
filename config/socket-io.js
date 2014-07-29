99cc9101266048bd2f79984ca46cad87f21ea9b7
Jordi Llobet <jordi@kentlyons.com>
2014-07-29 15:35:12 +0100

    changes to configurate de sockets

--- config/socket-io.js
+++ config/socket-io.js
@@ -2,6 +2,10 @@ module.exports = function(app, server, secret) {
     var clients = {};
     console.log("initiating sockets...");
     var sio = require('socket.io').listen(server);
+    sio.configure(function () { 
+        sio.set("transports", ["xhr-polling"]); 
+        sio.set("polling duration", 10); 
+    });
 
     sio.sockets.on('connection', function (socket) {
         clients[socket.id] = socket;
